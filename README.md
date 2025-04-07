5de5cc90309d864b57ad742a21f6695db7466840
Eldiiar Bekbolotov <eldiiarbekbolotov@gmail.com>
2025-04-07 18:58:20 -0400

    v1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mathstudy
+
 Collection of High School Math Course Notes by Eldiiar Bekbolotov
+
+First released on Replit late fall 2023 originally for a Business class assignment.
+
+Edited and re-released April 2025 using Netlify, mainly because Replit has removed the free tier of it's web hosting.
+
+However, I have decided not to commit to updating the lesson content on this website, so the grade 9 course will not be completed and grade 10, 11, 12 courses empty.
---
+++ mathunits/unitdata.js
@@ -0,0 +1,254 @@
+var unit1articles = [
+    {
+        title: "Unit 1: Numbers",
+        content: "Lessons 1.1 - 1.10<br>September 7 - September 27",
+        type: "Tutorial",
+        href:"#"
+    },
+    {
+        title: "1.1 - Number Sets",
+        content: `<P>Here's a summary of numbers:
+<br><br>
+Whole Numbers are numbers without decimal parts. Cannot be fractional or negative numbers.<br>
+e.g. 0,1,2,3,4,5,6,7,8,9,10
+<br><br>
+Integers are all whole numbers, including negative whole numbers.<br>
+e.g. -5,-4,-3,-2,-1,0,1,2,3,4,5
+<br><br>
+Rational Numbers are numbers which can be written as a fraction or any ratio. Dividing one integer by another integer will give you a rational number. These can go on forever, as long as they repeat.
+<br>e.g. 1/2, 1/3, 0.5, 0.3333333..., 0.15151515..., 9.32, 932/100
+<br><br>
+Irrational Numbers go on forever but are non-repeating. If you can't write a number as a simple fraction, it is an irrational number.<br>
+e.g. Pi (3.1415926...), √2 (1.4142135), √5 (2.23606797749...)
+<br><br>
+Real Numbers are all of the numbers you can find on a number line. Whole numbers, integers, rational and irrational numbers are all a subset of real numbers.<br>
+e.g. 123, -7, Pi, 0.987231, -412, 9, 12, 3, 0, 1</p>`,
+        by: "LeaferStudios",
+        date: "2021-12-26",
+        type: "Article",
+    },
+    {
+        title: `1.2 - Density, Limits, and Infinity`,
+        content: `<p><b>Density</b> property means we can always find a real number between two real numbers. There are infinitely many real numbers between two numbers. </p><p>A <b>Limit</b> is the value we’re approaching.<br><a href="https://www.khanacademy.org/math/ap-calculus-ab/ab-limits-new/ab-1-2/a/limits-intro" target="_blank">Limits intro (article) | Khan Academy</a><br><a href='https://www.mathsisfun.com/calculus/limits.html' target='_blank'>Limits (An introduction)</a></p>
+<p><b>Infinity</b> is the state of having no limit. Can always be made larger than any given value. It is not a number.</p>
+`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: `1.3 - Powers, Order of Operations`,
+        content: `<p><b>Powers</b> are a way of writing repeated multiplication. The <i>base</i> tells us which value to repeatedly multiply, and the <i>exponent</i> tells us how many times to perform the multiplication.<br>e.g. 3<sup>4</sup> = 3x3x3x3 = 81</p>
+<p><b>Order of Operations</b> tells us which part of an expression with multiple operations we calculate first. There is a simple acronym for figuring out the right order of operations called <b>BEDMAS</b> (sometimes also referred to as PEDMAS):</p>
+<ol>
+    <li>Brackets (Parantheses), everything inside them</li>
+    <li>Exponents</li>
+    <li>Division, left to right</li>
+    <li>Multiplication, left to right</li>
+    <li>Addition, left to right</li>
+    <li>Subtraction, left to right</li>
+</ol>`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: `1.4 - Algebraic Notation`,
+        content: `<p>Algebra is a branch of math in which letters and symbols are used to represent numbers and the relationships between numbers.<br><br><b>Algebraic Notation</b> is a way of writing math expressions using symbols, letters, and numbers. It's used to present information in a concise manner.</p>
+
+<p style='margin-left: 20px;font-size: 15px'>Glossary of Algebra:<br><br>Equations – a mathematical statement that two expressions are equal <br><br>
+Evaluate – to substitute a value for each variable in an expression, then simplify: to find the answer <br><br>
+Expression – a mathematical phrase made up of numbers and or variables, connected by operations <br><br>
+Operation – a mathematical process or action such as addition, subtraction, multiplication, or division <br><br>
+Variable – a letter or symbol that represents a number or quantity that can take on different values.<br><br>
+Term (in an expression) – one of the things being added or subtracted. 
+</p>`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+
+    {
+        title: `1.5 - Scientific Notation`,
+        content: `<b>Scientific Notation</b> is a way of writing really large or really small numbers. It's written in numbers between 1 and 10, muliplied by 10 raised by some power.<br><br><b>To write large numbers (greater than 10) in scientific notation</b>:<br><br>1. Move the decimal place left until you get a number in between 1 and 10.<br>2. Multiply that number by 10 to the power of the number of times you had to move the decimal place to the left.<br><br> <b>Example</b>: 729,000 in scientific notation. To get to 7.29, we have to move the decimal point 5 times to the left. Therefore, 729,000 is 7.29x10<sup>5</sup><br><br><b>To write small numbers (less than 1) in scientific notation:</b><br><br>1. Move the decimal place right until you get a number in between 1 and 10.<br>2. Multiply that number by 10 to the power of negative the number of times you had to move the decimal place to the right.<br><br><b>Example</b>: 0.0013 in scientific notation. To get to 1.3, we have to move the decimal point 3 times to the right. Therefore, 0.0013 is 1.3x10<sup>-3</sup> in scientific notation.<br><br><br> The normal way of writing a number instead of scientific notation is called Standard Notation.`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+
+    {
+        title: `1.6 - Rational Numbers`,
+        content: `
+                    <b>Rational Numbers</b>: a type of real number. Basically any fraction with a non-zero denominator is a rational number. Examples: 0/1, 1/1, 3/4, 1923123/2, 9/12. <br><br><b>Converting between mixed numbers and improper fractions</b>: 
+                    `,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: `1.7 - Multiplying and Dividing Fractions`,
+        content: `<b>Multiplying Fractions:</b> Pretty simple, you multiply the numerators and you multiply the denominators and simplify if necessary. For example:<br>5/6 x 3/8 = 5x3/6x8 = 15/48 = 5/16 simplified.<br><b>Dividing Fractions</b>: First change the division sign to multiplication. Next flip the second fraction so that the numerator and denominator are swapped. Multiply the fraction and simplify if necessary. For example:<br>1/2 divided by 3/4 = 1/2 multiplied by 4/3 = 1x4/2x3 = 4/6 = 2/3 simplified.</b>`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: `1.8 - Adding and Subtracting Fractions`,
+        content: `<b>Adding Fractions:</b>
+                    To add fractions with the same denominators, simply add the numerators and keep the denominator the same.
+                    <br>
+                    Example: 3/9 + 1/9 = 4/9
+                    <br>If the denominators are not the same, make them the same by finding the Lowest Common Denominator. You can only add fractions if they have the same denominators.
+                    <br>
+                    Example: <br>1/4+1/3
+                    <br>The Lowest Common Denominator of 4 and 3 is 12. Therefore, we have to change the fractions so that the denominator is 12.
+                    <br>
+                    3/12 + 4/12 = 7/12
+                    <br>
+                    So 1/4+1/3 = 7/12
+                    <br>
+                    
+                    <b>Subtracting Fractions</b>: To subtract fractions with the same denominators, simply subtract the numerators and keep the denominator the same.<br>Example: 5/9 - 1/9 = 4/9<br>If the denominators are not the same, make them the same by finding the Lowest Common Denominator. Then, subtract the numerators. You can only subtract fractions with the same denominators.<br>Example: 10/20 - 2/40 = 10/20 - 1/20 = 9/20`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: `1.9 - Ratio and Proportions`,
+        content: `<b>Ratio</b>: How many times one number contains another. Can be expressed in several different ways:<br>1) Fraction (5/7, 10/20)<br>2) Colon (5:7, 10:20)<br>3) Word Form (5 to 7, 10 to 20)<br>To simplify a ratio, divide each part of the ratio by the Greatest Common Factor (GCF).<br><br>Example: To simplify the ratio 16:4, we must first find the Greatest Common Factor (GCF). The GCF of 16 and 4 is 4, so we must divide both numbers of the ratio by 4. 16/4 = 4 and 4/4 = 1. So 16:4 simplified is equal to 4:1.<br><br><b>Proportion</b>: A statement saying two ratios are equal. For example: <br>a) 3:5 = 6:10<br>b) 6:18 = 3:9<br>c) 4/16 = 2/8<br>d) 70 to 10 = 35 to 5<br><br>The cross product property of proportions states that the cross products of two ratios will be equal if the two ratios form a proportion.<br>Example: a/b = c/d<br>b x c = a x d<br><br>a/4 = 6/8<br>To solve this problem, we multiply the means and the extremes.<br>a x 8 = 4 x 6<br>a x 8 = 24<br>Then, using inverse operation we can figure out what the variable <em>a</em> is equal to.<br>8a/8 = 24/8<br>a = 3`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: `1.10 - Rates and Percent`,
+        content: `<b>Rate</b>: A comparison of two quantities with different units. A <b>unit rate</b> is when the second quantitiy is one unit. For example, you eat 10 apples in 1 hour. That is a unit rate, since it is measuring how many apples you ate in one unit (1 hour). Another example of a rate would be you earn $50 in 2 hours. That is not a unit rate, since it is measuring how much money you make in two units (2 hours).<br><br><b>Percent</b>: A ratio out of 100. Think of the word 'per hundred'. It can be expressed as a decimal or a fraction.<br>Example: 9/16 = 0.5625 = 56.25%<br>Example: 3/50 = 0.06 = 6%<br>Example: 4.32/10 = 0.432 = 43.2%`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+
+    /**{
+        title: ``,
+        content: ``,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: "",
+        link: "",
+        about: "PJS",
+        type: "Tutorial",
+        by: "",
+    },**/
+
+];
+var unit2articles = [
+    {
+        title: "Unit 2: Polynomials",
+        content: "Lessons 2.1 - 2.8<br>September 28 - October 17",
+        type: "Tutorial",
+        href:"#"
+    },
+{
+        title: `2.1 Polynomials`,
+        content: `Poly comes from the Greek word meaning "many" and nomials comes from the Greek word meaning "terms". Polynomials means "many terms".<br><br>The x occurring in a polynomial is commonly called a variable or an indeterminate. When the polynomial is considered as an expression, x is a fixed symbol which does not have any value (its value is "indeterminate"). However, when one considers the function defined by the polynomial, then x represents the argument of the function, and is therefore called the "variable". Many authors use these two words interchangeably.<br><br>The exponent on an indeterminate in a term is called the degree of that indeterminate in that term; the degree of the term is the sum of the degrees of the indeterminates in that term, and the degree of a polynomial is the largest degree of any term with nonzero coefficient. Because x = x1, the degree of an indeterminate without a written exponent is one.`,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    
+{
+        title: `2.2 Collecting Like Terms`,
+        content: `Like terms are summands in a sum that differ only by a numerical factor. Like terms can be regrouped by adding their coefficients. Typically, in a polynomial expression, like terms are those that contain the same variables to the same powers, possibly with different coefficients.
+<br><br>
+More generally, when some variable are considered as parameters, like terms are defined similarly, but "numerical factors" must be replaced by "factors depending only on the parameters".<br><br>A "term" will refer to a string of numbers being multiplied or divided (that division is simply multiplication by a reciprocal) together. Terms are within the same expression and are combined by either addition or subtraction. `,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    /**{
+        title: ``,
+        content: ``,
+        by: "LeaferStudios",
+        date: "2023-09-25",
+        type: "Article",
+    },
+    {
+        title: "",
+        link: "",
+        about: "PJS",
+        type: "Tutorial",
+        by: "",
+    },**/
+
+];
+
+
+var scenes = document.getElementsByClassName("scene");
+        var switchPage = function(index) {
+            for (var scene = 0; scene < scenes.length; scene++) {
+                scenes[scene].style.display = "none";
+            }
+            window.scroll(0, 0);
+            scenes[index].style.animation = "0.75s forwards fade-in";
+            scenes[index].style.display = "block";
+        };
+        switchPage(0);
+
+ var openArticle = function(z, a) {
+            switchPage(1);
+            document.getElementById("content").innerHTML = `
+            <button onclick='switchPage(0);' style='height: 36px;padding: 0 16px;background-color:dodgerblue;border-radius:3px;border:none;font-family:"Google Sans",trebuchet ms, arial;color:#FFF;float:left;margin-right:32px;margin-top:10px;'>Back</button>
+            <h1 style="text-align:center;">${a[z].title}</h1>
+            
+            <p style='padding-top:35px;'>${a[z].content}</p>
+            `;
+        };
+
+var allArticles = document.getElementById("all-articles");
+var loadNews = function(j, k) {
+    document.getElementById("all-articles").innerHTML = "";
+    
+    for (var i = 0; i < j.length; i++) {
+        var at = document.createElement("a");
+        switch (j[i].type) {
+            case "Tutorial":
+                at.innerHTML = `
+                        <a href=${j[i].href} style="text-decoration:none;">
+                        <div class="article-at" style='background-color:#E3F2FD;border-color:#42A5F5;'>
+                        <h3>${j[i].title}</h3>
+                        <p style='color:#000;'>${j[i].content}</p>
+                        </div>
+                        </a>
+                        `;
+                break;
+                 case "Article":
+                if (k === 0) {
+         at.innerHTML = `
+                        <a href="javascript:openArticle(${i}, unit1articles);" style="text-decoration:none;">
+                        <div class="article-at">
+                        <h3>${j[i].title}</h3>
+                        <p>by ${j[i].by}</p>
+                        </div>
+                        </a>
+                        `;
+    } else if (k === 1) {
+                     at.innerHTML = `
+                        <a href="javascript:openArticle(${i}, unit2articles);" style="text-decoration:none;">
+                        <div class="article-at">
+                        <h3>${j[i].title}</h3>
+                        <p>by ${j[i].by}</p>
+                        </div>
+                        </a>
+                        `;
+    }
+                       
+                    break;
+        }
+
+        allArticles.innerHTML += at.innerHTML;
+    }
+};
+
